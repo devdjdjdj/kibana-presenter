e16bb4963493809f905a584c46679b4327b38ffd
Devansh Jaiswal <8934007+devdjdjdj@users.noreply.github.com>
2020-05-08 02:24:06 -0500

    single quote

--- pages/api/templates.js
+++ pages/api/templates.js
@@ -1,21 +1,21 @@
-import { getTemplates, addTemplate } from "../../lib/controller"
-import { runMiddleware } from "../../lib/middleware"
-const morgan = require("morgan")
+import { getTemplates, addTemplate } from '../../lib/controller'
+import { runMiddleware } from '../../lib/middleware'
+const morgan = require('morgan')
 
 export default async (req, res) => {
-  await runMiddleware(req, res, morgan("tiny"))
+  await runMiddleware(req, res, morgan('tiny'))
   switch (req.method) {
-    case "GET":
+    case 'GET':
       const allTemplates = getTemplates()
       if (allTemplates.length) {
         res.statusCode = 200
         res.json(allTemplates)
       } else {
         res.statusCode = 404
-        res.json({ message: "No Templates" })
+        res.json({ message: 'No Templates' })
       }
       break
-    case "POST":
+    case 'POST':
       const { templates, code } = addTemplate(req.body)
       res.statusCode = code
       res.json(templates)
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head"
+import Head from 'next/head'
 
 export default function Home() {
   return <div className="container">Skeleton</div>
