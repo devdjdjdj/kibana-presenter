e74b9c95f06f6730d42b6a641650d922467eaf51
Devansh Jaiswal <8934007+devdjdjdj@users.noreply.github.com>
2020-05-08 02:20:10 -0500

    no semi

--- pages/api/tabs.js
+++ pages/api/tabs.js
@@ -1,24 +1,24 @@
-import { getTabs, addTab } from "../../lib/controller";
-import { runMiddleware } from "../../lib/middleware";
-const morgan = require("morgan");
+import { getTabs, addTab } from "../../lib/controller"
+import { runMiddleware } from "../../lib/middleware"
+const morgan = require("morgan")
 
 export default async (req, res) => {
-  await runMiddleware(req, res, morgan("tiny"));
+  await runMiddleware(req, res, morgan("tiny"))
   switch (req.method) {
     case "GET":
-      let allTabs = getTabs();
+      let allTabs = getTabs()
       if (allTabs.length) {
-        res.statusCode = 200;
-        res.json(allTabs);
+        res.statusCode = 200
+        res.json(allTabs)
       } else {
-        res.statusCode = 404;
-        res.json({ message: "No Tabs" });
+        res.statusCode = 404
+        res.json({ message: "No Tabs" })
       }
-      break;
+      break
     case "POST":
-      const { tabs, code } = addTab(req.body);
-      res.statusCode = code;
-      res.json(tabs);
-      break;
+      const { tabs, code } = addTab(req.body)
+      res.statusCode = code
+      res.json(tabs)
+      break
   }
-};
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from "next/head";
+import Head from "next/head"
 
 export default function Home() {
-  return <div className="container">Skeleton</div>;
+  return <div className="container">Skeleton</div>
 }
