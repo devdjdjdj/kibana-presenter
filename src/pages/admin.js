8c0424c204da311d90b482f602f57a8653705bab
Devansh Jaiswal <8934007+devdjdjdj@users.noreply.github.com>
2020-05-09 13:42:57 -0500

    Admin skeleton

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,5 +1,24 @@
-export default function () {
-  return (<div className="container">
-    Admin Console
-  </div>)
+import { getTabs, getTemplates } from '../lib/controller'
+
+export default function ({ tabs, templates }) {
+  return (
+    <div className="container">
+      <div>
+        Tabs :
+        {tabs.map((e, i) => <div key={i}> {e.name} </div>)}
+      </div>
+      <div>
+        Templates
+        {templates.map((e, i) => <div key={i}> {e.name} </div>)}
+      </div>
+      <div>
+        Add New Tab
+      </div>
+    </div>)
+}
+
+export async function getServerSideProps() {
+  const tabs = getTabs()
+  const templates = getTemplates()
+  return { props: { tabs, templates } }
 }
