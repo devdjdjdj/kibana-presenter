ba7243aca68609a0c72bb94b511c7d3bab0c4713
Devansh Jaiswal <8934007+devdjdjdj@users.noreply.github.com>
2020-05-09 03:03:25 -0500

    route to admin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
-import Head from 'next/head'
+import Router from 'next/router'
+import {getTabs, getTemplates} from '../lib/controller' 
 
-export default function Home() {
-  return <div className="container">Skeleton</div>
+export default function ({tabs, templates}) {
+  React.useEffect(() => {
+    if (!tabs.length) {
+      Router.push('/admin')
+    }
+  }) 
+  return (<div className="container">
+    { tabs.map((e,i) => <div key={i}> {e.name} </div>)}
+  </div>)
+}
+
+export async function getServerSideProps() {
+  const tabs = getTabs()
+  const templates = getTemplates()
+  return { props: {tabs, templates}}
 }
