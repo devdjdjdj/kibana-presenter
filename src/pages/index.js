ff30573f3083074abc00e5a104e51432b3c7880a
Devansh Jaiswal <8934007+devdjdjdj@users.noreply.github.com>
2020-05-10 16:59:42 -0500

    index tabs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,36 @@
-import Router from 'next/router'
-import { getTabs, getTemplates } from '../lib/controller'
+import Router from "next/router";
+import { getTabs, getTemplates } from "../lib/controller";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/core";
 
-export default function ({ tabs, templates }) {
+export default function({ tabs, templates }) {
   React.useEffect(() => {
     if (!tabs.length) {
-      Router.push('/admin')
+      Router.push("/admin");
     }
-  })
+  });
 
   return (
     <div className="container">
-      {tabs.map((e, i) => (
-        <div key={i}> {e.name} </div>
-      ))}
+      <Tabs>
+        <TabList>
+          {tabs.map((tab, index) => (
+            <Tab key={index}>{tab.name}</Tab>
+          ))}
+        </TabList>
+        <TabPanels>
+          {tabs.map((tab, index) => (
+            <TabPanel p={4} key={index}>
+              {JSON.stringify(tab.data)}
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
     </div>
-  )
+  );
 }
 
 export async function getServerSideProps() {
-  const tabs = getTabs()
-  const templates = getTemplates()
-  return { props: { tabs, templates } }
+  const tabs = getTabs();
+  const templates = getTemplates();
+  return { props: { tabs, templates } };
 }
