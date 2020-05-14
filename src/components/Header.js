b488d7ad40efbb96a6aa47f7725ed392aa5bc16e
Devansh Jaiswal <8934007+devdjdjdj@users.noreply.github.com>
2020-05-14 00:19:50 -0500

    AppBar

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Heading, Flex } from '@chakra-ui/core'
+
+export const Header = ({tabTitle}) => {
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1rem"
+      bg="cyan.600"
+      color="white"
+      >
+      <Flex align="center" mr={5}>
+        <Heading as="h1" size="lg">
+          Kibana Presenter {tabTitle}
+        </Heading>
+      </Flex>
+    </Flex>
+  )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,15 @@
 import { ThemeProvider, CSSReset } from '@chakra-ui/core'
+import { Header } from '../components/Header'
 
-export default ({ Component, pageProps }) => (
-  <ThemeProvider>
+export default ({ Component, pageProps }) => {
+  const [tabTitle, setTabTitle] = React.useState("")
+  const handleTabChange = (title) => {
+    console.log("Tab changed"+title)
+    setTabTitle(` - Current Tab : ${title}`)
+  }
+  return(<ThemeProvider>
     <CSSReset />
-    <Component {...pageProps} />
-  </ThemeProvider>
-)
+    <Header tabTitle={tabTitle}/>
+    <Component {...pageProps} changeHeaderDisplay={handleTabChange}/>
+  </ThemeProvider>)
+}
